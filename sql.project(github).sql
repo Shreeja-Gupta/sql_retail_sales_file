43b881fc496d64a4ebdecc13c9c67323968c9bf7
Shreeja-Gupta <shreejagupta4@gmail.com>
2025-04-28 12:43:27 +0530

    Update sql.project(github).sql

--- sql.project(github).sql
+++ sql.project(github).sql
@@ -1,5 +1,5 @@
 
-**1. Write a SQL query to retrieve all columns for sales made on '2022-11-05:**
+**1.Write a SQL query to retrieve all columns for sales made on '2022-11-05:**
 
 select * from sql_project_p1.retail_sales
 where sale_date ='2022-11-05';
